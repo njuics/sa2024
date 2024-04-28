5d6064f509904a06a72d1e34a78f70a823d1c12b
Chun <caochun@gmail.com>
2024-04-28 09:48:06 +0800

    update

---
+++ 07-microservices/assets/8-CDMyScij.js
@@ -0,0 +1,8 @@
+import{o as t,c as r,k as n,g as a,q as i,s as c,A as s,e}from"./modules/vue-uEYNCD1x.js";import{I as l}from"./slidev/default-Ce4Bd5Wd.js";import{_ as m,ad as o}from"./index-fVPylRdL.js";import{p,u as _,f as d}from"./slidev/context-9wIGX_xO.js";import"./modules/shiki-BzAJ3tB6.js";const u="/sa2024/07-microservices/assets/06-mono-vs-micro-D7-72azJ.png",f=e("h1",null,"Monoliths vs. Microservice",-1),h=e("br",null,null,-1),v=e("img",{src:u,class:"m-10 h-70"},null,-1),g={__name:"8",setup(x){return p(o),_(),(y,k)=>(t(),r(l,i(c(s(d)(s(o),7))),{default:n(()=>[f,h,v,a(` ---
+
+# 集成发布
+
+![height:400px](images/06-microservice-ci.png)
+
+
+<small><small>Micro services can only be deployed independently if tests are independent & if your Continuous Delivery Pipeline works</small></small> `)]),_:1},16))}},w=m(g,[["__file","/@slidev/slides/8.md"]]);export{w as default};
